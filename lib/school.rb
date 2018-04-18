6fe3d78e6c9d25a2f13a2468fbbdc2e27eacfb7b
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-18 23:50:50 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,8 @@
 class School
-  attr_accessor :school, :name
+  attr_accessor :school
 
-  def initialize(name="")
-    @name = name
+  def initialize(school="")
+    @school = school
   end
 
   def rooster
